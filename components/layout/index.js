d6dff747b7a579113ce7430bdb9dab79c22e5571
fuhao <465476662@qq.com>
2018-03-17 21:31:42 +0800

    解决冲突

--- components/layout/index.js
+++ components/layout/index.js
@@ -6,7 +6,10 @@ import {
 	Breadcrumb,
 	Row,
 	Col,
-	Button
+	Button,
+	Radio,
+	AutoComplete,
+	Select,
 } from 'antd';
 import Base from '../base/index.js';
 import commonStyle from '../../common/style/index.css';
@@ -19,13 +22,25 @@ const {
 	Content
 } = Layout;
 
+const RadioButton = Radio.Button;
+const RadioGroup = Radio.Group;
+
+const Option = Select.Option;
+
 export default class extends React.Component {
 	constructor(props) {
 		super(props);
 
 		this.state = {
-			menuContentKey: ''
+			menuContentKey: '',
+			schoolChoose: 'name',
+			professorChoose: 'proName',
+			rateChoose: 'presonal'
 		};
+
+		this.onRadioChangeHandler = this.onRadioChangeHandler.bind(this);
+		this.onSelectChangeHandler = this.onSelectChangeHandler.bind(this);
+		this.onInputChangeHandler = this.onInputChangeHandler.bind(this);
 	}
 
 	componentDidMount() {
@@ -44,25 +59,342 @@ export default class extends React.Component {
 		});
 	}
 
-	renderMenuContent(key) {
-		switch(key) {
+	onRadioChangeHandler(e) {
+		const {
+			menuContentKey
+		} = this.state;
+
+		switch (menuContentKey) {
 			case '2':
-				return (
+				this.setState({
+					schoolChoose: e.target.value,
+				});
+				return;
+			case '3':
+				this.setState({
+					professorChoose: e.target.value,
+				});
+				return;
+			case '4':
+				this.setState({
+					rateChoose: e.target.value,
+				});
+				return;
+		}
+	}
+
+	onSelectChangeHandler(e) {
+		console.log(e);
+	}
+
+	onInputChangeHandler(e) {
+		console.log(e);
+	}
+
+	renderFindSchool() {
+		const {
+			schoolChoose
+		} = this.state;
+
+		return (
+			<div className={style.menuWrap}>
+				<Row className={style.row}>
+					<Col
+						className={cla(commonStyle.textCenter)}
+						span={12}
+						offset={6}>
+						查找你的学校
+					</Col>
+				</Row>
+
+				<Row className={style.row}>
+					<Col
+						className={cla(commonStyle.textCenter)}
+						span={12}
+						offset={6}>
+						<RadioGroup onChange={this.onRadioChangeHandler} value={schoolChoose}>
+			        <RadioButton value="name">按名称</RadioButton>
+			        <RadioButton value="region">按区域</RadioButton>
+			      </RadioGroup>
+					</Col>
+				</Row>
+
+				{
+					schoolChoose == 'name' ?
+					<Row className={style.row}>
+						<Col
+							className={cla(commonStyle.textCenter)}
+							span={12}
+							offset={6}>
+				        <AutoComplete
+				        	style={{ width: 200, height: 32 }}
+				        	placeholder="学校名称"
+				        	onChange={this.onInputChangeHandler}/>
+						</Col>
+					</Row>
+					:
+					<Row className={style.row}>
+						<Col
+							className={cla(commonStyle.textCenter)}
+							span={12}
+							offset={6}>
+				        <Select
+				        	placeholder="请选择区域"
+				        	style={{ width: 200, height: 32 }}
+				        	onChange={this.onSelectChangeHandler}>
+						      <Option value="上海">上海</Option>
+						      <Option value="北京">北京</Option>
+						      <Option value="广州">广州</Option>
+						      <Option value="新疆">新疆</Option>
+						    </Select>
+						</Col>
+					</Row>
+				}
+
+				<Row className={style.row}>
+					<Col
+						className={cla(commonStyle.textCenter)}
+						span={12}
+						offset={6}>
+			        <Button style={{ width: 200 }} type="primary">搜索</Button>
+					</Col>
+				</Row>
+
+				<Row className={style.row}>
+					<Col
+						className={cla(commonStyle.textCenter)}
+						span={12}
+						offset={6}>
+			        <Button style={{ width: 200 }}>取消</Button>
+					</Col>
+				</Row>
+			</div>
+		);
+	}
+
+	renderFindProfessor() {
+		const {
+			professorChoose
+		} = this.state;
+
+		return (
+			<div className={style.menuWrap}>
+				<Row className={style.row}>
+					<Col
+						className={cla(commonStyle.textCenter)}
+						span={12}
+						offset={6}>
+						查找你的教授
+					</Col>
+				</Row>
+
+				<Row className={style.row}>
+					<Col
+						className={cla(commonStyle.textCenter)}
+						span={12}
+						offset={6}>
+						<RadioGroup onChange={this.onRadioChangeHandler} value={professorChoose}>
+			        <RadioButton value="proName">按名称</RadioButton>
+			        <RadioButton value="school">按学校</RadioButton>
+			      </RadioGroup>
+					</Col>
+				</Row>
+
+				{
+					professorChoose == 'proName' ?
 					<div>
-						<Row gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }}>
+						<Row className={style.row}>
 							<Col
 								className={cla(commonStyle.textCenter)}
 								span={12}
 								offset={6}>
-								查找你的学校
+					        <div>I'm looking for a professor at</div>
+							</Col>
+						</Row>
+
+						<Row className={style.row}>
+							<Col
+								className={cla(commonStyle.textCenter)}
+								span={12}
+								offset={6}>
+					        <AutoComplete
+					        	style={{ width: 200, height: 32 }}
+					        	placeholder="学校名称"
+					        	onChange={this.onInputChangeHandler}/>
+							</Col>
+						</Row>
+
+						<Row className={style.row}>
+							<Col
+								className={cla(commonStyle.textCenter)}
+								span={12}
+								offset={6}>
+					        <div>named</div>
+							</Col>
+						</Row>
+
+						<Row className={style.row}>
+							<Col
+								className={cla(commonStyle.textCenter)}
+								span={12}
+								offset={6}>
+					        <AutoComplete
+					        	style={{ width: 200, height: 32 }}
+					        	placeholder="教授名字"
+					        	onChange={this.onInputChangeHandler}/>
 							</Col>
 						</Row>
 					</div>
-				);
+					:
+					<div>
+						<Row className={style.row}>
+							<Col
+								className={cla(commonStyle.textCenter)}
+								span={12}
+								offset={6}>
+									I'm looking for a professor at
+					        <AutoComplete
+					        	style={{ width: 200, height: 32, paddingLeft: 4 }}
+					        	placeholder="学校名字"
+					        	onChange={this.onInputChangeHandler}/>
+							</Col>
+						</Row>
+
+						<Row className={style.row}>
+							<Col
+								className={cla(commonStyle.textCenter)}
+								span={12}
+								offset={6}>
+									in the
+					        <Select
+					        	placeholder="请选择学校"
+					        	style={{ width: 200, height: 32, paddingLeft: 4, paddingRight: 4 }}
+					        	onChange={this.onSelectChangeHandler}>
+							      <Option value="学校1">学校1</Option>
+							      <Option value="学校2">学校2</Option>
+							      <Option value="学校3">学校3</Option>
+							      <Option value="学校4">学校4</Option>
+							    </Select>
+							    department
+							</Col>
+						</Row>
+					</div>
+				}
+
+				<Row className={style.row}>
+					<Col
+						className={cla(commonStyle.textCenter)}
+						span={12}
+						offset={6}>
+			        <Button style={{ width: 200 }} type="primary">搜索</Button>
+					</Col>
+				</Row>
+
+				<Row className={style.row}>
+					<Col
+						className={cla(commonStyle.textCenter)}
+						span={12}
+						offset={6}>
+			        <Button style={{ width: 200 }}>取消</Button>
+					</Col>
+				</Row>
+			</div>
+		);
+	}
+
+	renderComment() {
+		const {
+			rateChoose
+		} = this.state;
+
+		return (
+			<div className={style.menuWrap}>
+				<Row className={style.row}>
+					<Col
+						className={cla(commonStyle.textCenter)}
+						span={12}
+						offset={6}>
+						我要点评
+					</Col>
+				</Row>
+
+				<Row className={style.row}>
+					<Col
+						className={cla(commonStyle.textCenter)}
+						span={12}
+						offset={6}>
+						<RadioGroup onChange={this.onRadioChangeHandler} value={rateChoose}>
+			        <RadioButton value="professor">教授</RadioButton>
+			        <RadioButton value="school">学校</RadioButton>
+			      </RadioGroup>
+					</Col>
+				</Row>
+
+				<Row className={style.row}>
+					<Col
+						className={cla(commonStyle.textCenter)}
+						span={12}
+						offset={6}>
+			        <div>I want to rate</div>
+					</Col>
+				</Row>
+
+				{
+					rateChoose == 'professor' ?
+					<Row className={style.row}>
+						<Col
+							className={cla(commonStyle.textCenter)}
+							span={12}
+							offset={6}>
+				        <AutoComplete
+				        	style={{ width: 200, height: 32 }}
+				        	placeholder="教授名字"
+				        	onChange={this.onInputChangeHandler}/>
+						</Col>
+					</Row>
+					:
+					<Row className={style.row}>
+						<Col
+							className={cla(commonStyle.textCenter)}
+							span={12}
+							offset={6}>
+				        <AutoComplete
+				        	style={{ width: 200, height: 32 }}
+				        	placeholder="学校名称"
+				        	onChange={this.onInputChangeHandler}/>
+						</Col>
+					</Row>
+				}
+
+				<Row className={style.row}>
+					<Col
+						className={cla(commonStyle.textCenter)}
+						span={12}
+						offset={6}>
+			        <Button style={{ width: 200 }} type="primary">搜索</Button>
+					</Col>
+				</Row>
+
+				<Row className={style.row}>
+					<Col
+						className={cla(commonStyle.textCenter)}
+						span={12}
+						offset={6}>
+			        <Button style={{ width: 200 }}>取消</Button>
+					</Col>
+				</Row>
+			</div>
+		);
+	}
+
+	renderMenuContent(key) {
+		switch(key) {
+			case '2':
+				return this.renderFindSchool();
 			case '3':
-				return <div>3333</div>;
+				return this.renderFindProfessor();
 			case '4':
-				return <div>4444</div>;
+				return this.renderComment();
 			default:
 				return <div></div>;
 		}
@@ -80,32 +412,32 @@ export default class extends React.Component {
 
 		return (
 			<Base title={title}>
-  				<Layout>
-	  				<Header className={style.header}>
-				        <h2 className={style.logo}>桥选®校园</h2>
-				        <Menu
-				          theme="dark"
-				          mode="horizontal"
-				          className={style.menu}
-				          onClick={item => this.MenuClickHandler(item)}>
+  			<Layout>
+  				<Header className={style.header}>
+			        <h2 className={style.logo}>桥选®校园</h2>
+			        <Menu
+			          theme="dark"
+			          mode="horizontal"
+			          className={style.menu}
+			          onClick={item => this.MenuClickHandler(item)}>
 
-					        <Menu.Item key="1">关于我们</Menu.Item>
-					        <Menu.Item key="2">查找高校</Menu.Item>
-					        <Menu.Item key="3">查找教授</Menu.Item>
-					        <Menu.Item key="4">我要点评</Menu.Item>
-				        </Menu>
-						  <a href={this.props.url.query.loginUrl}><Button>登录</Button></a>
-				    </Header>
+				        <Menu.Item key="1">关于我们</Menu.Item>
+				        <Menu.Item key="2">查找高校</Menu.Item>
+				        <Menu.Item key="3">查找教授</Menu.Item>
+				        <Menu.Item key="4">我要点评</Menu.Item>
+			        </Menu>
+					  <a href={this.props.url.query.loginUrl}><Button>登录</Button></a>
+			    </Header>
 
 					{menuContentKey && this.renderMenuContent(menuContentKey)}
 
-    				{children}
+    			{children}
 
 					<Footer style={{ textAlign: 'center' }}>
-        				Ant Design ©2016 Created by Ant UED
-      				</Footer>
+        		Ant Design ©2016 Created by Ant UED
+      		</Footer>
 				</Layout>
-  			</Base>
+  		</Base>
 		);
 	}
 }
