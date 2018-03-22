b97eb9a647eae807b72ee53b2b682d8800400e47
fuhao <465476662@qq.com>
2018-03-22 16:10:12 +0800

    整理api

--- common/api.js
+++ common/api.js
@@ -6,39 +6,54 @@ function makeApi(defaultOpt, opt) {
 
 module.exports = {
   /**
-   *
+   * 获取所有学生
    * @param opt
    */
-
-  getStudent: opt => makeApi({ url: '/api/get-student' }, opt),
-
-  /**
-   *
-   * @param opt
-   */
-  getSchoolGroupByCountry: opt => makeApi({ url: '/api/get-school-group-by-country' }, opt),
-
-  /**
-   *
-   * @param opt
-   */
-  createSchool: opt => makeApi({
-    url: '/api/school/create',
-    type: 'POST'
+  getStudent: opt => makeApi({
+    url: '/api/get-student'
   }, opt),
 
+  /**
+   * 根据国家获取学校
+   * @param opt
+   */
+  getSchoolGroupByCountry: opt => makeApi({
+    url: '/api/get-school-group-by-country'
+  }, opt),
+
+  /**
+   * 获取所有国家
+   * @param opt
+   */
   getAllCountry: opt => makeApi({
     url: '/open-api/geo/get-all-countrys'
   }, opt),
 
+  /**
+   * 根据国家获取省/州
+   * @param opt
+   */
   getProvinceByCountry: opt => makeApi({
     type: 'POST',
     url: '/open-api/geo/get-province-by-country'
   }, opt),
 
+  /**
+   * 根据省/州获取城市
+   * @param opt
+   */
   getCityByProvince: opt => makeApi({
     type: 'POST',
     url: '/open-api/geo/get-city-by-province',
+  }, opt),
+
+  /**
+   * 创建学校
+   * @param opt
+   */
+  createSchool: opt => makeApi({
+    type: 'POST',
+    url: '/api/school/create',
   }, opt),
 
   /**
@@ -52,15 +67,8 @@ module.exports = {
    * 根据条件获取学校
    * @param opt 参数包括（关键字，国家，省/州，城市Id）
    */
-  getSchoolByCondition: opt => makeApi({ url: '/api/get-school-by-condition' }, opt),
-
-  /**
-   * 创建教授
-   * @param opt
-   */
-  createProfessor: opt => makeApi({
-    url: '/api/professor/create',
-    type: 'POST'
+  getSchoolByCondition: opt => makeApi({
+    url: '/api/get-school-by-condition'
   }, opt),
 
   /**
@@ -68,8 +76,8 @@ module.exports = {
    * @param opt
    */
   getCollegeBySchool: opt => makeApi({
+    type: 'POST',
     url: '/api/get-college-by-school',
-    type: 'POST'
   }, opt),
 
   /**
@@ -77,8 +85,33 @@ module.exports = {
    * @param opt
    */
   createSchoolRate: opt => makeApi({
+    type: 'POST',
     url: '/api/school-rate/create',
-    type: 'POST'
+  }, opt),
+
+  /**
+   * 创建教授
+   * @param opt
+   */
+  createProfessor: opt => makeApi({
+    type: 'POST',
+    url: '/api/professor/create',
+  }, opt),
+
+  /**
+   * 获取教授详细信息
+   */
+  getSchoolDetail: opt => makeApi({
+    url: '/open-api/get-professor-detail',
+  }, opt),
+
+  /**
+   * 创建教授评价
+   * @param opt
+   */
+  createSchoolRate: opt => makeApi({
+    type: 'POST',
+    url: '/api/professor-rate/create',
   }, opt),
 
 };
