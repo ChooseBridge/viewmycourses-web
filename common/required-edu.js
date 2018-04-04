924f159a149d8eeb8c8f2157260c42b94006ce38
fuhao <465476662@qq.com>
2018-04-04 22:51:13 +0800

    修改edu邮箱为高校邮箱

--- common/required-edu.js
+++ common/required-edu.js
@@ -1,8 +1,8 @@
 const { Modal } = require('antd');
 
 export default () => Modal.confirm({
-  title: '请验证edu邮箱',
-  content: '该功能需要验证edu邮箱后使用',
+  title: '请验证高校邮箱',
+  content: '该功能需要验证高校邮箱后使用',
   okText: '前往修改邮箱',
   cancelText: '取消',
   onOk() {
