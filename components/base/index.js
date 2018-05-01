06833dce0f7ef38bc3e24c86fc7ff5dd4ce65465
fuhao <465476662@qq.com>
2018-05-01 12:13:21 +0800

    去除响应式

--- components/base/index.js
+++ components/base/index.js
@@ -3,7 +3,7 @@ import Head from 'next/head';
 export default ({ children, title, className }) =>
   <div className={className}>
     <Head>
-      <meta name='viewport' content='width=device-width, initial-scale=1' />
+      <meta name='viewport' content='width=device-width' />
       <meta charSet='utf-8' />
       <title>{title && `${title} | `}桥选校园</title>
       <link rel='stylesheet' href='https://unpkg.com/antd@3/dist/antd.min.css' />
