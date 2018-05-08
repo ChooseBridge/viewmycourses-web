19418b180cc98f68e6eb1d9a5b19fde4a4f08386
Zehao Zhang <zehao93@outlook.com>
2018-05-08 09:40:15 +0800

    modal样式

--- pages/school/rate.js
+++ pages/school/rate.js
@@ -86,6 +86,7 @@ class SchoolRate extends React.Component {
             title: '创建成功',
             content: '感谢您的点评，为保护您的隐私，点评将会在审核通过后延时发出。',
             okText: '确定',
+            width: 550,
             onOk: () => history.back()
           });
         }, e => {
