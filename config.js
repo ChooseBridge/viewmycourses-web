65985960e4fa9317c59f60ebd45618b5d23e3c27
root <root@choosebridge.com>
2018-06-01 10:57:46 +0800

    Update config.js

--- config.js
+++ config.js
@@ -5,8 +5,8 @@ const DEV = !TEST && !PROD;
 
 const config = {
   port: 3000,
-  apiHost: 'http://school.anyquestion.top',
-  loginUrl: 'https://i.choosebridge.com/oauth/authorize?client_id=1&redirect_uri=http://school.anyquestion.top/callback&response_type=code&state=',
+  apiHost: 'https://api.viewmycourses.com/',
+  loginUrl: 'https://i.choosebridge.com/oauth/authorize?client_id=999&redirect_uri=https://api.viewmycourses.com/callback&response_type=code&state=',
   loginRedirectPrefix: 'https://www.viewmycourses.com'
 };
 
